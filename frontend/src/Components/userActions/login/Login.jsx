6b841b2875cef672a8b8e591d3da1c2e9100fcf7
Amaan Khan <amaankhaniit@gmail.com>
2023-09-12 03:37:42 +0530

    migrated to redux , also functionalities in register page

--- frontend/src/Components/userActions/login/Login.jsx
+++ frontend/src/Components/userActions/login/Login.jsx
@@ -1,42 +1,50 @@
 import "./Login.css";
-import Register from "../register/Register";
-import cancel from "../../../assets/cancel.png";
 
-const Login = ({ close, showReg }) => {
+import cancel from "../../../assets/cancel.png";
+import { blogActions } from "../../../store/store";
+import { useDispatch } from "react-redux";
+
+const Login = () => {
+  const dispatch = useDispatch();
+
   const loginFormHanndler = (e) => {
     e.preventDefault();
   };
   const closeUserActionHandler = () => {
-    close();
+    dispatch(blogActions.closeAuth());
   };
 
   const showRegHandler = () => {
-    showReg();
+    dispatch(blogActions.showLoginModal(false));
+    dispatch(blogActions.showRegistrationModal(true));
   };
-  return (
-    <section className="login_cont">
-      <div className="login_cont__icon" onClick={closeUserActionHandler}>
-        <img src={cancel} alt="cancel" />
-      </div>
-      <div className="login_cont__msg">Welcome back!</div>
-      <form onSubmit={loginFormHanndler} className="login_cont__form">
-        <section>
-          {/* <label>Enter Username</label> */}
-          <input type="text" placeholder="Enter Username" />
-        </section>
 
-        <section>
-          {/* <label>Enter Password</label> */}
-          <input type="password" placeholder="Enter Password" />
-        </section>
-        <button type="submit" className="login_cont_formButton">
-          Login
-        </button>
-      </form>
-      <div className="login_cont__otherOp" onClick={showRegHandler}>
-        No account? Create one
-      </div>
-    </section>
+  return (
+    <>
+      <section className="login_cont">
+        <div className="login_cont__icon" onClick={closeUserActionHandler}>
+          <img src={cancel} alt="cancel" />
+        </div>
+        <div className="login_cont__msg">Welcome back!</div>
+        <form onSubmit={loginFormHanndler} className="login_cont__form">
+          <section>
+            {/* <label>Enter Username</label> */}
+            <input type="text" placeholder="Enter Username" />
+          </section>
+
+          <section>
+            {/* <label>Enter Password</label> */}
+            <input type="password" placeholder="Enter Password" />
+          </section>
+          <button type="submit" className="login_cont_formButton">
+            Login
+          </button>
+        </form>
+        <div className="login_cont__otherOp" onClick={showRegHandler}>
+          No account? Create one
+        </div>
+      </section>
+    </>
   );
 };
 
--- frontend/src/Components/userActions/register/Register.jsx
+++ frontend/src/Components/userActions/register/Register.jsx
@@ -1,10 +1,19 @@
 import "../login/Login.css";
-import Login from "../login/Login";
 import cancel from "../../../assets/cancel.png";
-import { useState } from "react";
+import { blogActions } from "../../../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
-const Register = ({ close }) => {
-  const [formData, setFormData] = useState({ username: "", password: "" });
+const Register = () => {
+  const dispatch = useDispatch();
+  const usernameExists = useSelector((state) => state.usernameExists);
+  const passNotEqual = useSelector((state) => state.passNotEqual);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const usernameHandler = (event) => {
     setFormData((prev) => {
@@ -12,31 +21,69 @@ const Register = ({ close }) => {
     });
   };
 
+  const [username, setUsernames] = useState({});
+
+  useEffect(() => {
+    dispatch(blogActions.usernameExists(false));
+    dispatch(blogActions.setPassNotEqual(false));
+    //logic for checking if username exists or not
+    let timer = setTimeout(async () => {
+      const usernamesRes = await fetch("http://localhost:3000/users/usernames");
+      const data = await usernamesRes.json();
+      setUsernames(() => data);
+      data.forEach((el) => {
+        if (el.username == formData.username) {
+          dispatch(blogActions.usernameExists(true));
+        }
+      });
+    }, 800);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [formData]);
+
   const passwordHandler = (event) => {
     setFormData((prev) => {
       return { ...prev, password: event.target.value };
     });
   };
-  const loginFormHanndler = (e) => {
-    e.preventDefault();
-    console.log("this ran");
-    //mylogic for unique username everytime
-    const jsonRes = async () => {
-      const req = await fetch("http://localhost:3000/users/register", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const res = await req.json();
-      console.log(res);
-    };
-    jsonRes();
+
+  const confirmPasswordHandler = (event) => {
+    setFormData((prev) => {
+      return { ...prev, confirmPassword: event.target.value };
+    });
   };
+
+  const loginFormHandler = (e) => {
+    dispatch(blogActions.setPassNotEqual(false));
+    e.preventDefault();
+    if (formData.password != formData.confirmPassword) {
+      dispatch(blogActions.setPassNotEqual(true));
+    } else {
+      const jsonRes = async () => {
+        const req = await fetch("http://localhost:3000/users/register", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const res = await req.json();
+        console.log(res);
+      };
+      // jsonRes();
+    }
+  };
+
   const closeUserActionHandler = () => {
-    close();
+    dispatch(blogActions.closeAuth());
+  };
+
+  const showLog = () => {
+    dispatch(blogActions.showLoginModal(true));
+    dispatch(blogActions.showRegistrationModal(false));
   };
 
   return (
@@ -45,7 +92,7 @@ const Register = ({ close }) => {
         <img src={cancel} alt="cancel" />
       </div>
       <div className="login_cont__msg">Welcome!</div>
-      <form onSubmit={loginFormHanndler} className="login_cont__form">
+      <form onSubmit={loginFormHandler} className="login_cont__form">
         <section>
           {/* <label>Enter Username</label> */}
           <input
@@ -53,7 +100,11 @@ const Register = ({ close }) => {
             placeholder="Enter Username"
             onChange={usernameHandler}
             value={formData.username}
+            required
           />
+          {usernameExists && (
+            <div className="login_cont__error">username already exists</div>
+          )}
         </section>
 
         <section>
@@ -63,18 +114,28 @@ const Register = ({ close }) => {
             placeholder="Enter Password"
             onChange={passwordHandler}
             value={formData.password}
+            required
           />
         </section>
         <section>
           {/* <label>Enter Password</label> */}
-          <input type="password" placeholder="Confirm Password" />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            onChange={confirmPasswordHandler}
+            value={formData.confirmPassword}
+            required
+          />
         </section>
+        {passNotEqual && (
+          <div className="login_cont__error">Passwords do not match.</div>
+        )}
         <button type="submit" className="login_cont_formButton">
           Register
         </button>
       </form>
-      <div className="login_cont__otherOp">
-        Already have a account? <a href={<Login />}>Login instead</a>
+      <div className="login_cont__otherOp" onClick={showLog}>
+        Already have a account? Login instead
       </div>
     </section>
   );
